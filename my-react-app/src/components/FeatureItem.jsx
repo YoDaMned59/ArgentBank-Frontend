3893610ec75a301a38f6f2eb369105ccc1a1955d
YoDaMned59 <sduvivier59112@gmail.com>
2025-02-20 14:17:02 +0100

    refactor: extract Features data into a separate file

---
+++ my-react-app/src/components/FeatureItem.jsx
@@ -0,0 +1,11 @@
+
+
+export const FeatureItem = ({ icon, alt, title, description }) => {
+  return (
+    <div className="feature-item">
+      <img src={icon} alt={alt} className="feature-icon" />
+      <h3 className="feature-item-title">{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+};
